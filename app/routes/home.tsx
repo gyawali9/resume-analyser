6319218930d727a839f9daa28ab7a93a68fb25a0
gyawali9 <roshangyawali9@gmail.com>
2026-02-11 14:30:54 +0545

    feat(ui): build homepage with navbar and resume card

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,36 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { RESUMES_LIST } from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Pulse" },
+    { name: "description", content: "Pulsify your resume!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Know the pulse of your resume and job progress.</h1>
+          <h2>
+            Get insights into your job applications and improve your resume
+            scores.
+          </h2>
+        </div>
+
+        {RESUMES_LIST.length > 0 && (
+          <div className="resumes-section">
+            {RESUMES_LIST.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
